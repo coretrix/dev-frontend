955ebf57d7054d92fe84136e41731f30f8a73085
Yavor Vasilev <qvor.vasilev@gmail.com>
2021-02-12 12:06:18 +0200

    Change layout

--- pages/redis-stats/details.vue
+++ pages/redis-stats/details.vue
@@ -1,10 +1,30 @@
 <template>
   <div>
     <v-btn to="/redis-stats" class="mr-4 white--text" color="blue">Back</v-btn>
-    <pre class="mt-5">
-    {{ JSON.stringify(pool, null, 2) }}
-  </pre
-    >
+    <v-card v-if="pool" class="mt-5">
+      <v-card-title>
+        <h1 class="m-0 mb-5">{{ pool.RedisPool }}</h1>
+      </v-card-title>
+      <v-card-text>
+        <table class="text-left table">
+          <thead>
+            <tr>
+              <th>Key</th>
+              <th>Value</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="entry in Object.entries(pool.Info)" :key="entry[0]">
+              <td>{{ entry[0] }}</td>
+              <td>{{ entry[1] }}</td>
+            </tr>
+          </tbody>
+        </table>
+      </v-card-text>
+    </v-card>
+    <v-card v-else class="mt-5">
+      <v-card-text>Erm...no data in JSON?</v-card-text>
+    </v-card>
   </div>
 </template>
 
@@ -92,4 +112,16 @@ export default {
   },
 }
 </script>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.table {
+  width: 100%;
+  border: 1px solid #ccc;
+  border-collapse: collapse;
+
+  td,
+  th {
+    padding: 4px 8px;
+    border: 1px solid #ccc;
+  }
+}
+</style>
