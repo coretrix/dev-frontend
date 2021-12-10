2ea992b462c6752482a491f56fa4e7d972caa5b0
Yavor <qvor.vasilev@gmail.com>
2021-12-10 12:43:50 +0200

    Feature flag - confirmation modal

--- pages/feature-flag.vue
+++ pages/feature-flag.vue
@@ -1,99 +1,103 @@
 <template>
-  <v-data-table
-    :headers="headers"
-    :items="items"
-    :loading="isLoading"
-    loading-text="Loading... Please wait"
-    class="elevation-1"
-    :items-per-page="15"
-    fixed-header
-    :height="$vuetify.breakpoint.height - 200"
-    sort-by="Name"
-    sort-desc
-    must-sort
-    item-key="ID"
-    :hide-default-footer="!items.length"
-    :footer-props="{ 'items-per-page-options': [5, 15, 50, 100, -1] }"
-  >
-    <template #top>
-      <v-toolbar flat>
-        <div class="text-center d-flex align-center justify-space-around">
+  <div>
+    <v-data-table
+      :headers="headers"
+      :items="items"
+      :loading="isLoading"
+      loading-text="Loading... Please wait"
+      class="elevation-1"
+      :items-per-page="15"
+      fixed-header
+      :height="$vuetify.breakpoint.height - 200"
+      sort-by="Name"
+      sort-desc
+      must-sort
+      item-key="ID"
+      :hide-default-footer="!items.length"
+      :footer-props="{ 'items-per-page-options': [5, 15, 50, 100, -1] }"
+    >
+      <template #top>
+        <v-toolbar flat>
+          <div class="text-center d-flex align-center justify-space-around">
+            <v-tooltip bottom color="grey darken-1" content-class="py-1">
+              <template v-slot:activator="{ on, attrs }">
+                <v-btn
+                  color="primary"
+                  small
+                  dark
+                  class="mr-2"
+                  v-bind="attrs"
+                  v-on="on"
+                >
+                  <v-icon size="18px" @click="getErrorsList(false)">
+                    {{ icons.mdiRefresh }}
+                  </v-icon>
+                </v-btn>
+              </template>
+              <span class="white--text text-caption">Refresh</span>
+            </v-tooltip>
+          </div>
+        </v-toolbar>
+      </template>
+
+      <template #item.lp="{ index, item }">
+        <div
+          class="d-flex fill-height align-center mx-n4 px-4 py-1"
+          :class="{
+            'red white--text': !item.Registered && item.Enabled
+          }"
+        >
+          {{ ++index }}
+        </div>
+      </template>
+
+      <template #item.Name="{ item }">
+        <div
+          class="d-flex fill-height align-center mx-n4 px-4 py-1"
+          :class="{
+            'red white--text': !item.Registered && item.Enabled
+          }"
+        >
+          {{ item.Name }}
+        </div>
+      </template>
+
+      <template #item.Registered="{ item }">
+        <div
+          class="d-flex fill-height align-center mx-n4 px-4 py-1"
+          :class="{
+            'red white--text': !item.Registered && item.Enabled
+          }"
+        >
+          <v-icon v-if="item.Registered" color="green">
+            {{ icons.mdiCheckCircle }}
+          </v-icon>
+          <v-icon v-else color="white">
+            {{ icons.mdiCloseCircle }}
+          </v-icon>
+        </div>
+      </template>
+
+      <template #item.Enabled="{ item }">
+        <div
+          class="d-flex fill-height align-center mx-n4 px-4 py-1"
+          :class="{
+            'red white--text': !item.Registered && item.Enabled
+          }"
+        >
           <v-tooltip bottom color="grey darken-1" content-class="py-1">
             <template v-slot:activator="{ on, attrs }">
-              <v-btn
-                color="primary"
-                small
-                dark
-                class="mr-2"
-                v-bind="attrs"
-                v-on="on"
-              >
-                <v-icon size="18px" @click="getErrorsList(false)">
-                  {{ icons.mdiRefresh }}
-                </v-icon>
-              </v-btn>
+              <div>
+                <v-switch v-model="item.Enabled" readonly @click="toggleFeature(item)" />
+              </div>
             </template>
-            <span class="white--text text-caption">Refresh</span>
+            <span class="white--text text-caption">Toggle feature</span>
           </v-tooltip>
         </div>
-      </v-toolbar>
-    </template>
-
-    <template #item.lp="{ index, item }">
-      <div
-        class="d-flex fill-height align-center mx-n4 px-4 py-1"
-        :class="{
-          'red white--text': !item.Registered && item.Enabled
-        }"
-      >
-        {{ ++index }}
-      </div>
-    </template>
-
-    <template #item.Name="{ item }">
-      <div
-        class="d-flex fill-height align-center mx-n4 px-4 py-1"
-        :class="{
-          'red white--text': !item.Registered && item.Enabled
-        }"
-      >
-        {{ item.Name }}
-      </div>
-    </template>
-
-    <template #item.Registered="{ item }">
-      <div
-        class="d-flex fill-height align-center mx-n4 px-4 py-1"
-        :class="{
-          'red white--text': !item.Registered && item.Enabled
-        }"
-      >
-        <v-icon v-if="item.Registered" color="green">
-          {{ icons.mdiCheckCircle }}
-        </v-icon>
-        <v-icon v-else color="white">
-          {{ icons.mdiCloseCircle }}
-        </v-icon>
-      </div>
-    </template>
-
-    <template #item.Enabled="{ item }">
-      <div
-        class="d-flex fill-height align-center mx-n4 px-4 py-1"
-        :class="{
-          'red white--text': !item.Registered && item.Enabled
-        }"
-      >
-        <v-tooltip bottom color="grey darken-1" content-class="py-1">
-          <template v-slot:activator="{ on, attrs }">
-            <!-- v-model="switch1" -->
-            <v-switch v-model="item.Enabled" @change="toggleFeature(item)" />
-          </template>
-          <span class="white--text text-caption">Toggle feature</span>
-        </v-tooltip>
-      </div>
-    </template>
-  </v-data-table>
+      </template>
+    </v-data-table>
+    <CoreConfirmation ref="confirmationModal" />
+  </div>
 </template>
 
 <script lang="ts">
@@ -103,8 +107,9 @@ import {
   mdiCloseCircle
   , mdiClose
 } from '@mdi/js'
-import { Component, mixins } from 'nuxt-property-decorator'
+import { Component, mixins, Ref } from 'nuxt-property-decorator'
 import { ApiUtilities } from '~/components/mixins/Global'
+import Confirmation from '@/components/core/Confirmation.vue'
 
 type FeatureItem = {
   Name:string,
@@ -124,6 +129,7 @@ export default class FeatureFlag extends mixins(ApiUtilities) {
   headers:Object[] = []
   items:Object[] = []
   isLoading:boolean = false
+  isToggleLoading = {}
 
   test = false
 
@@ -169,8 +175,18 @@ export default class FeatureFlag extends mixins(ApiUtilities) {
       .then(this.apiOnFinishRequest)
   }
 
+  @Ref('confirmationModal') readonly confirmationModal!: Confirmation
   toggleFeature (item:FeatureItem) {
-    !item.Enabled ? this.disableFeature(item) : this.enableFeature(item)
+    this.confirmationModal
+      ?.show({
+        title: 'Wait!!!',
+        message: 'Are you sure you want to proceed? It cannot be undone.'
+      })
+      .then((result: any) => {
+        if (result) {
+          item.Enabled ? this.disableFeature(item) : this.enableFeature(item)
+        }
+      })
   }
 
   enableFeature (item:FeatureItem) {
